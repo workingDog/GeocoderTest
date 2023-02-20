f9c75e5df3c2727e4e02d7e3b9eb56964cb753d5
workingdog <ringowathelet@gmail.com>
2023-02-20 17:16:14 +0900

    Initial Commit

---
+++ GeocoderTest/GeocoderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeocoderTestApp.swift
+//  GeocoderTest
+//
+//  Created by Ringo Wathelet on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct GeocoderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
